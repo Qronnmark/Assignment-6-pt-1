9598abb7f323d0944687c865f793a57930400331
Qronnmark <q.ronnmark@sjasd.ca>
2017-01-20 16:14:57 -0600

    Finished Code.
    GG.

---
+++ src/Arrayaveragemodified.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author q.ronnmark
+ *
+ */
+public class Arrayaveragemodified {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Methods.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+
+/**
+ * @author q.ronnmark
+ * Quenton Ronnmark
+ * Mr.Hardman
+ * Assignment 6 Part 1
+ * Dec. 9th 2016
+ */
+	public class Methods {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		displayName();
+		displayBirthday();
+		displayFact();
+		
+	
+	}
+			
+			
+	public static void displayName() {
+		
+		
+		System.out.println("My name is Quenton.");
+		
+	}
+	
+			
+	public static void displayBirthday() {
+		
+		
+		System.out.println("My birthday is November 5th.");
+		
+	}
+			
+	
+	public static void displayFact() {
+	
+		System.out.println("A fact about me is that I love Final Fantasy and the best games are 15 and 7.");
+		
+	}
+
+			
+			
+			
+			
+			
+			
+			
+	}
